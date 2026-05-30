fbdfda35e505425d88587f43fc407cad7547ca12
SebasUNI-256 <rodrigo.rodriguez231173@udem.edu.ni>
2026-05-30 08:08:54 -0600

    MIgracion, y creacion de schema para la base de datos

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -4,7 +4,10 @@ class Product < ApplicationRecord
     validates :name, presence:true, lenght: {minimum:3}
     #se asegura de que el precio nunca sea 0
     validates :price, presence:true, numericality: {greather_than: 0}
-    #se asegura de que solo se puedan meter numero enteros y que siempre sean igual o mayor a 0
+    #se asegura de que solo se puedan meter numeros enteros y que siempre sean igual o mayor a 0
     validates :stock, presence: true, numericality:{only_integer: true, greather_than_or_equals: 0}
     validates :category, presence:true
 end
+
+#Presenece true quiere decir que no puede queda vacio
+#Numeracality quiere decir que ese campo especifacamente solo acepta numeros
---
+++ db/migrate/20260530043309_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :stock
+      t.boolean :active
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
